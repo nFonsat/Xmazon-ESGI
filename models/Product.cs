e837c675620cdcd535c740bb363f02771e014b5f
nourben <nour.benharkat@gmail.com>
2016-02-25 18:23:51 +0100

    nouveaux import
    avancement view products

--- models/Product.cs
+++ models/Product.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using Newtonsoft.Json;
+using System.Json;
 
 namespace XmazonProject
 {
 	public class Product
 	{
-		public Product(String uid, String name, float price, Boolean available)
+		public Product(String uid, String name, float price, bool available)
 		{
 			this.uid = uid;
 			this.name = name;
@@ -24,7 +26,17 @@ namespace XmazonProject
 
 		public float price { private set; get; }
 
-		public Boolean available { private set; get; }
+		public bool available { private set; get; }
+
+		public static Product Deserialize (JsonValue jsonCategory)
+		{
+			string uid = jsonCategory ["uid"];
+			string name = jsonCategory ["name"];
+			float price = (float)jsonCategory ["price"];
+			bool available = (bool)jsonCategory ["available"];
+
+			return new Product (uid, name, price, available);
+		}
 	}
 }
 
---
+++ models/ProductResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Json;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace XmazonProject
+{
+	public class ProductResponse
+	{
+		public ProductResponse ()
+		{
+		}
+
+		public ProductResponse (int code, List<Product> result)
+		{
+			this.code = code;
+			this.result = result;
+		}
+
+		public int code { get; set; }
+		public List<Product> result { get; set; }
+	
+		public static ProductResponse Deserialize (JsonValue jsonCategory)
+		{
+			String json = jsonCategory.ToString ();
+//			JsonArray result = jsonCategory ["result"];
+
+			return JsonConvert.DeserializeObject<ProductResponse>(json);
+		}
+	}
+}
+
