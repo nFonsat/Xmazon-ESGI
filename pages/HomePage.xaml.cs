2dd4c289376ab4d50200592b5b2b9c3f1672c907
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-02-25 17:52:37 +0100

    implement logout

--- pages/HomePage.xaml.cs
+++ pages/HomePage.xaml.cs
@@ -26,11 +26,31 @@ namespace XmazonProject
 			gotoStoreList ();
 		}
 
+		void logoutAction(object sender, EventArgs ea){
+			TokenManager.Instance.DeleteToken (OAuthContext.AppContext);
+			TokenManager.Instance.DeleteToken (OAuthContext.UserContext);
+			goToLoginPage ();
+		}
+
 		void gotoStoreList ()
 		{
 			Device.BeginInvokeOnMainThread(() =>  {
 				this.Navigation.PushAsync(new StoreListView ());
 			});
+		}
+
+		private void goToLoginPage ()
+		{
+			Device.BeginInvokeOnMainThread(() =>  {
+				ReplaceRootAsync (new LoginPage ());
+			});
+		}
+
+		private void ReplaceRootAsync(Page page)
+		{
+			NavigationPage navigation = new NavigationPage(page);
+			App.Current.MainPage = navigation;
+			this.Navigation.PopToRootAsync();
 		}
 	}
 }
