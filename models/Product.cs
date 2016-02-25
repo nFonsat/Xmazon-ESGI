84d4e3e70f86acfffff45ecbdb661a27d89a7fcf
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-02-25 15:42:14 +0100

    implement Product created by Nour

--- models/Product.cs
+++ models/Product.cs
@@ -4,9 +4,27 @@ namespace XmazonProject
 {
 	public class Product
 	{
-		public Product ()
+		public Product(String uid, String name, float price, Boolean available)
 		{
+			this.uid = uid;
+			this.name = name;
+			this.price = price;
+			this.available = available;
 		}
+
+		public Product(String uid, String name)
+		{
+			this.uid = uid;
+			this.name = name;
+		}
+
+		public String uid { private set; get; }
+
+		public String name { private set; get; }
+
+		public float price { private set; get; }
+
+		public Boolean available { private set; get; }
 	}
 }
 
